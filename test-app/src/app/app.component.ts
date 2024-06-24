7e6b0f4018cb8ee19923c2045ae0a923c6293c93
Oleg Granevskij <o.granevskij@gmail.com>
2024-06-24 05:58:00 +0300

    Added some rules

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  // @typescript-eslint/array-type
+  aaa: string[] = [];
+  aaa1: Array<string> = []; // err
+
+  a() {
+    // for (let index = 0; index < array.length; index++) {
+    //   const element = array[index];
+
+    // }
+
+    debugger;
+
+    // typeof this.aaa === "string"
+
+    () => 5;
+    () => {
+      const a = 4;
+      return 5 + a;
+    };
+
+    const foo = [1,2,3];
+
+    for (const i in foo) {
+      console.log(i);
+    }
+
+    for (const i in foo) {
+    }
+
+    // this.defaultParm(1, ,5);
+  }
+
+  defaultParm(a: number, b = false, c: number) {
+    return a || b || c;
+  }
 }
